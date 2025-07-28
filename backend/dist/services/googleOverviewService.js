c3ac06b609759830d5637bd81f1bf4b0e48f3cef
Maxx Millstein <millsteinm@gmail.com>
2025-07-28 12:19:49 -0500

    added remote db config

--- backend/dist/services/googleOverviewService.js
+++ backend/dist/services/googleOverviewService.js
@@ -12,7 +12,8 @@ async function fetchGoogleOverview(query) {
     try {
         const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CX}&q=${encodeURIComponent(query)}`;
         const response = await axios_1.default.get(url);
-        const items = response.data.items;
+        const data = response.data;
+        const items = data.items;
         if (items && items.length > 0) {
             // Return the snippet from the first result
             return items[0].snippet;
@@ -28,7 +29,8 @@ async function fetchTrailImages(query) {
     try {
         const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${GOOGLE_CX}&q=${encodeURIComponent(query)}&searchType=image&num=6`;
         const response = await axios_1.default.get(url);
-        const items = response.data.items;
+        const data = response.data;
+        const items = data.items;
         if (items && items.length > 0) {
             // Return the image links
             return items.map((item) => item.link);
